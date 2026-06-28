42883f4e55e57d81cf744fc05a82345680d397d4
Birchi <noreply@bi.code>
2026-06-28 21:07:25 +0200

    Updating debug log

--- scripts/ansible/build.sh
+++ scripts/ansible/build.sh
@@ -85,5 +85,5 @@ fi
 
 log INFO "Building ansible galaxy collection ${namespace_name}.${collection_name}."
 cd ${collection_directory_path} && ansible-galaxy collection build --force --output-path ${output_file_path} > /dev/null 2>&1
-log INFO "Built ansible galaxy collection ${namespace_name}.${collection_name}."
+log DEBUG "Built ansible galaxy collection ${namespace_name}.${collection_name}."
 
